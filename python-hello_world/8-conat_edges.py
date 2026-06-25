89b99fe88192dfa5039da498644dc5d480a764a2
airumva2-sudo <a.irumva2@alustudent.com>
2026-06-25 05:29:44 -0700

    concat 8

--- python-hello_world/8-conat_edges.py
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/python3
-str = "Python is an interpreted, interactive, object-oriented programming\laguage that combines remarkable power with very clear syntax"
-print(str[39:66] + str[6:7] + str[106:110] + str[6:7] + str[0:6])
