042b6e3115443151ed0400e60139a074fcea14c8
airumva2-sudo <airumva2@alustudent.com>
2026-07-31 09:41:26 -0700

    inheritance

--- python-inheritance/4-inherits_from.py
+++ python-inheritance/4-inherits_from.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""Defines a function that checks if an object's class inherited from another class."""
+"""Defines a function that checks if an object inherited from a class."""
 
 
 def inherits_from(obj, a_class):
