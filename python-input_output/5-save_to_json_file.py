2b916193d23ae744a89657231ec8f6ff13ca09a0
airumva2-sudo <airumva2@alustudent.com>
2026-07-31 11:04:18 -0700

    save

---
+++ python-input_output/5-save_to_json_file.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""Defines a function that saves an Object to a file as JSON."""
+import json
+
+
+def save_to_json_file(my_obj, filename):
+    """Write an Object to a text file, using a JSON representation.
+
+    Args:
+        my_obj: The object to serialize.
+        filename (str): The name of the file to write to.
+    """
+    with open(filename, "w", encoding="utf-8") as f:
+        json.dump(my_obj, f)
