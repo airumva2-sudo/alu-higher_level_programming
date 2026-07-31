b98977da332288dd20acde8a288021f3939bbf8a
airumva2-sudo <airumva2@alustudent.com>
2026-07-31 09:47:10 -0700

    updated

--- python-inheritance/3-is_kind_of_class.py
+++ python-inheritance/3-is_kind_of_class.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""Defines a function that checks if an object is an instance of a class or subclass."""
+"""Defines a function that checks if an object is a kind of a class."""
 
 
 def is_kind_of_class(obj, a_class):
